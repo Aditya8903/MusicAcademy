5ecf95f32bff122ed246bf8bbe5d4685a065b66d
Aditya Gupta <aditya892003@gmail.com>
2024-07-08 16:23:39 +0530

    featured courses component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+"use client";
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image"; // Import only if you're using it
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection/>
+      <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,14 +1,67 @@
+"use client";
 import React from "react";
-import courses from "../data/music_courses.json";
+import courseData from "../data/music_courses.json";
 import { BackgroundGradient } from "./ui/background-gradient";
-
+import Link from "next/link";
+//defing own datatype for course
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+  image: string;
+}
 function FeaturedCourses() {
-  return;
-  <div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-  </div>;
+  const courses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
+  return (
+    <div className="py-12">
+      <div>
+        <div className="text-center">
+          <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
+            FEATURED COURSES
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+            Learn With the Best
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center mt-16">
+        {courses.map((course: Course) => (
+          <div key={course.id} className="flex justify-center">
+            <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+              <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                  {course.title}
+                </p>
+                <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                  {course.description}
+                </p>
+                <Link
+                  href={`/courses/${course.slug}`}
+                  className="text-blue-400 mt-5"
+                >
+                  Learn More
+                </Link>
+              </div>
+            </BackgroundGradient>
+          </div>
+        ))}
+      </div>
+      <div className="mt-20 text-center">
+        <Link
+          href={"/courses"}
+          className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+        >
+          View All courses
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default FeaturedCourses;
