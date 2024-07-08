5f77c0464fd52bf3d8b04a398b497d784b825fd1
Aditya Gupta <aditya892003@gmail.com>
2024-07-08 15:52:59 +0530

    created navbar and hero section

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import courses from "../data/music_courses.json";
+import { BackgroundGradient } from "./ui/background-gradient";
+
+function FeaturedCourses() {
+  return;
+  <div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>;
+}
+
+export default FeaturedCourses;
